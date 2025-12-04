acba745b3b3ccb2f1fcc2aa9e10d36d717973b2d
imiiii <56634004+imiiii@users.noreply.github.com>
2025-12-04 18:58:49 +0100

    Add GitHub Pages deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GITHUB_PAGES === 'true' ? '/AniccaMenu/' : '/',
+  build: {
+    outDir: 'dist',
+  },
 })
 
